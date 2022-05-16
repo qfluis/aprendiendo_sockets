7ca4b74ebf431ae0b6a9ac1522f93ffeeb624b58
Luis Quevedo <qfluis@gmail.com>
2022-05-16 16:51:11 +0200

    sigue

--- backend/app/models/sockets.js
+++ backend/app/models/sockets.js
@@ -4,6 +4,8 @@ class Sockets {
     constructor( io ) {
 
         this.salas = new Salas();
+        // TODO: Listado usuarios x sala ¿?
+        // TODO: Actualizar lista usuarios
 
         this.io = io;
         this.socketEvents();
@@ -14,12 +16,9 @@ class Sockets {
         this.io.on('connection', ( socket ) => {
 
             console.log("Cliente conectado", socket.id);
+            socket.join(this.salas[0]); // Por defecto en primera sala (general)
 
             socket.emit('lista-salas', this.salas.getSalas());
-
-            socket.on('disconnect', () => {
-                console.log("Cliente desconectado", socket.id);
-            });
 
             socket.on('disconnect', () => {
                 console.log("Cliente desconectado", socket.id);
@@ -27,8 +26,11 @@ class Sockets {
            
             socket.on("entrar-sala", (sala) => {
                 socket.join(sala);
-                console.log(socket.id, "Entra en sala " + sala)
+                console.log(socket.id, "Entra en sala " + sala);
+                // emit("entrar-saña ¿?")
             });
+
+            //TODO: salir-sala¿?
 
             socket.on('enviar-mensaje', (data) => {
                 console.log(data);
