52fad3299b54650baca60e19934e5581ee5f6a9f
Luis Quevedo <qfluis@gmail.com>
2022-05-17 23:37:11 +0200

    Modelo usuario creado

--- backend/app/models/Usuario.js
+++ backend/app/models/Usuario.js
@@ -2,8 +2,37 @@ const { Schema, modle } = require('mongoose');
 const usuarioSchema = Schema({
     email: {
         type: String,
-        required: [true, 'El nombre es obligatorio']
+        required: [true, 'El email es obligatorio'],
+        unique: [true, 'Email existente']
+    },
+    pass: {
+        type: String,
+        required: [true, 'El email es obligatorio']
+    },
+    nickName: {
+        type: String,    
+        required: [true, 'nickName obligatorio'],
+        unique: [true, 'nickName no disponible']    
+    },
+    foto: {
+        type: String
+    },
+    fechaCreacion: {
+        type
+    },
+    rol: {
+        type:String,
+        required: true,
+        enum: ['ADMIN', 'USER']
+    },   
+    activo: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
     }
 });
 
-module.exports = ein;
+module.exports = model('Usuario', usuarioSchema);
