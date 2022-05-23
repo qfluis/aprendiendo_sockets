a678b0980c682ca43b0952b469719c63a22e3061
Luis Quevedo <qfluis@gmail.com>
2022-05-23 12:49:50 +0200

    peleando con Rutas React

--- frontend/src/SmapChatApp.js
+++ frontend/src/SmapChatApp.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { SocketProvider } from './context/SocketContext';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 export const SmapChatApp = () => {
     return (  
         <>
-            <SocketProvider>
-                <HomePage />
-            </SocketProvider>
+            <h2>Ola k ase</h2>
+            <Link to="/">HomePage</Link>
+            <Link to="/login">LoginPage</Link>
+           {/* <SocketProvider> */}
+           <BrowserRouter>
+                <Routes>
+                    <Route path="/" elem={<HomePage />} />
+                    <Route path="/login" elem={<LoginPage />} />              
+                    
+                </Routes>
+            </BrowserRouter>
+            {/*</SocketProvider>*/}
         </>
     );
 }
---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+
+function LoginPage() {
+
+
+
+    return (
+        <h1>Login</h1>
+    )
+
+}
+
+export default LoginPage;
