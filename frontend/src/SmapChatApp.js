91646ebb20039d70ff9ffc5afba01832d5f911dc
Luis Quevedo <qfluis@gmail.com>
2022-05-24 00:11:39 +0200

    Trabajando el login

--- frontend/src/SmapChatApp.js
+++ frontend/src/SmapChatApp.js
@@ -3,15 +3,18 @@ import { AppRouter } from './components/AppRouter';
 import { Footer } from './components/Footer';
 import { NavBar } from './components/NavBar';
 import { SocketProvider } from './context/SocketContext';
+import { AuthContext } from './auth/authContext';
 
 
 export const SmapChatApp = () => {
     return (  
         <>  
             <NavBar />
-            <SocketProvider>
-                <AppRouter />
-            </SocketProvider>        
+                <AuthContext.Provider>
+                <SocketProvider>
+                    <AppRouter />
+                </SocketProvider>
+                </AuthContext.Provider>        
             <Footer />
         </>
     );
